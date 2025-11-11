c6f217dedf3117899234c981cea1ce9714ea95aa
Mark Sto <marksto@yourown.software>
2025-11-12 01:07:50 +0400

    Update existing tests (new `protocols.clj` file)

--- test/resauce/core_test.clj
+++ test/resauce/core_test.clj
@@ -12,24 +12,26 @@
 
 (deftest test-resources
   (let [rs (sort (map str (resources "resauce")))]
-    (is (= (count rs) 2))
+    (is (= 2 (count rs)))
     (is (re-find #"src/resauce$" (first rs)))
     (is (re-find #"test/resauce$" (second rs)))))
 
 (deftest test-url-dir
   (testing "file URL"
-    (let [rs (url-dir (io/as-url (io/file "src/resauce")))]
-      (is (= (count rs) 1))
-      (is (re-matches #"file:.*src/resauce/core\.clj" (str (first rs))))))
+    (let [rs (sort (map str (url-dir (io/as-url (io/file "src/resauce")))))]
+      (is (= 2 (count rs)))
+      (is (re-matches #"file:.*src/resauce/core\.clj" (first rs)))
+      (is (re-matches #"file:.*src/resauce/protocols\.clj" (second rs)))))
   (testing "jar URL"
     (let [rs (sort (map str (url-dir (io/resource "medley"))))]
-      (is (= (count rs) 3))
+      (is (= 3 (count rs)))
       (is (re-matches #"jar:.*medley/core\.clj" (first rs)))
       (is (re-matches #"jar:.*medley/core\.cljs" (second rs)))
       (is (re-matches #"jar:.*medley/core\.cljx" (nth rs 2))))))
 
 (deftest test-resource-dir
   (let [rs (sort (map str (resource-dir "resauce")))]
-    (is (= (count rs) 2))
+    (is (= 3 (count rs)))
     (is (re-find #"src/resauce/core\.clj$" (first rs)))
-    (is (re-find #"test/resauce/core_test\.clj$" (second rs)))))
+    (is (re-find #"src/resauce/protocols\.clj$" (second rs)))
+    (is (re-find #"test/resauce/core_test\.clj$" (nth rs 2)))))
